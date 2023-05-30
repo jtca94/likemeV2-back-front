3ffff8e9ad91a9113cd039cb187c522e6ea485e4
jtcannobbio <tomas.cannobbio@gmail.com>
2023-05-30 14:01:10 -0400

    fixed issues

--- client/src/components/CardPost.jsx
+++ client/src/components/CardPost.jsx
@@ -1,41 +1,39 @@
 import TrashIcon from "./icons/TrashIcon";
 import HeartIcon from "./icons/heart-solid";
 
-export default function CardPost({ post, deletePostById, likePostById }) {
+export default function CardPost({post, deletePostById, likePostById}) {
   return (
-    <article className="card mb-4">
-      <img
-        src={post.img}
-        alt=""
-        className="card-img-top"
-      />
-      <div className="card-body">
-        <h5>{post.titulo}</h5>
-        <p>{post.descripcion}</p>
-        <div className="d-flex mt-3 justify-content-between align-items-center">
-          <div>
-            <HeartIcon
-              fill="red"
-              height="25"
-              onClick={() => {
-                likePostById(post.id);
-              }}
-              className="cursor-pointer"
-            />
-            <span className="ms-2">{post.likes}</span>
-          </div>
-          <div>
-            <TrashIcon
-              fill="red"
-              height="25"
-              onClick={() => {
-                deletePostById(post.id);
-              }}
-              className="cursor-pointer"
-            />
+    <div className="col-12 col-md-6">
+      <article className="card h-100">
+        <img src={post.img} alt="" className="card-img-top" />
+        <div className="card-body flex-column d-flex">
+          <h5>{post.titulo}</h5>
+          <p className="flex-grow-1">{post.descripcion}</p>
+          <div className="d-flex mt-3 justify-content-between align-items-center">
+            <div>
+              <HeartIcon
+                fill="red"
+                height="25"
+                onClick={() => {
+                  likePostById(post.id);
+                }}
+                className="cursor-pointer"
+              />
+              <span className="ms-2">{post.likes}</span>
+            </div>
+            <div>
+              <TrashIcon
+                fill="red"
+                height="25"
+                onClick={() => {
+                  deletePostById(post.id);
+                }}
+                className="cursor-pointer"
+              />
+            </div>
           </div>
         </div>
-      </div>
-    </article>
+      </article>
+    </div>
   );
 }
